2f74e8cd0dd72ac829c83522758f109c8b200c5e
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-05-14 14:36:21 +0200

    implemented lambda-functions

--- bisection.cpp
+++ bisection.cpp
@@ -1,47 +1,36 @@
 #include <iostream>
 #include <cmath>
+#include <functional>
 
-float f(float x)
-{
-    return exp(sin(x)) - 0.5;
-    // return 1/sqrt(pow(x,2) + 1) - cos(x+1);
-    // return sin(cos(x+1));
-    // return atan(pow(x,2) + 2*x) -0.2;
-    // return pow(x,2) + 1;
-}
+using namespace std;
 
-float bisec(float a, float b, float max_iter, float tol)
+float bisec(function<float(float)> func, float a, float b, float max_iter, float tol)
 {
-    if((f(a) > 0.0 && f(b) > 0.0) ||
-       (f(a) < 0.0 && f(b) < 0.0))
+    if((func(a) > 0.0 && func(b) > 0.0) ||
+       (func(a) < 0.0 && func(b) < 0.0))
     {
         return -1;
     }
 
     float retval;
 
-    std::cout << "a\t" << "b\t" << "retval\t" << "f(retval)\n";
-
     for(float k = 1; k <= max_iter; ++k)
     {
-        std::cout << "\n" << a << "\t" << b;
         
         retval = (a+b)*0.5;
 
-        std::cout << "\t" << retval << "\t" << f(retval) << std::endl;
-
-        if(fabs(f(retval)) < tol)
+        if(fabs(func(retval)) < tol)
         {
             break;
         }
 
-        if( (f(a) > 0.0 && f(retval) < 0.0) ||
-            (f(a) < 0.0 && f(retval) > 0.0))
+        if( (func(a) > 0.0 && func(retval) < 0.0) ||
+            (func(a) < 0.0 && func(retval) > 0.0))
         {
             b = retval;
         }
-        else if( (f(b) > 0.0 && f(retval) < 0.0) ||
-                 (f(b) < 0.0 && f(retval) > 0.0))
+        else if( (func(b) > 0.0 && func(retval) < 0.0) ||
+                 (func(b) < 0.0 && func(retval) > 0.0))
         {
             a = retval;
         }
@@ -57,8 +46,30 @@ int main()
     float max_iter = 50;
     float tol = 1e-7;
 
-    float bisecZero = bisec(a,b,max_iter,tol);
+    float bisecZero = bisec([](float x) -> float { return exp(sin(x)) - 0.5; },
+    a,b,max_iter,tol);
 
-    std::cout << bisecZero << " ist die gesuchte Nullstelle" << std::endl;
+    cout << bisecZero << " ist die gesuchte Nullstelle von f = exp(sin(x)) - 0.5 \n" << endl;
+
+    bisecZero = bisec([](float x) -> float { return 1/sqrt(pow(x,2) + 1) - cos(x+1); },
+    a,b,max_iter,tol);
+
+    cout << bisecZero << " ist die gesuchte Nullstelle von f = 1/sqrt(pow(x,2) + 1) - cos(x+1) \n" << endl;
+
+    bisecZero = bisec([](float x) -> float { return sin(cos(x+1)); },
+    a,b,max_iter,tol);
+
+    cout << bisecZero << " ist die gesuchte Nullstelle von f = sin(cos(x+1)) \n" << endl;
+
+    bisecZero = bisec([](float x) -> float { return atan(pow(x,2) + 2*x) -0.2; },
+    a,b,max_iter,tol);
+
+    cout << bisecZero << " ist die gesuchte Nullstelle von f = atan(pow(x,2) + 2*x) -0.2 \n" << endl;
+
+    bisecZero = bisec([](float x) -> float { return pow(x,2) + 1; },
+    a,b,max_iter,tol);
+
+    cout << bisecZero << " ist die gesuchte Nullstelle von f = pow(x,2) + 1 " << endl;
+
     return 0;
 }
