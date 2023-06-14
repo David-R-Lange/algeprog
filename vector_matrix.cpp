a7ea1b3d52a08c8c29dee3193d4765cb41f43b0b
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-06-14 20:13:14 +0200

    moved def. of objects into functions for better readability

--- vector_matrix.cpp
+++ vector_matrix.cpp
@@ -1,63 +1,68 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
 // Assignment 5.2
 
-int main() {
-    int const nv = 5;
-    int const n = 4;
-    int const m = 4;
-    int e_1[nv];  // Einheitsvektor e1 nach aufg. (i)
-    int c_1[nv];  // Einsvektor c1 aufg. (ii)
-    int v[nv];    // Vektor v def. in (iii)
-    int I[m * n]; // Einheitsmatrix auf. (iv)
-    int M[m * n]; // Matrix M def. in (v)
-
-    for (int i = 0; i <= nv; ++i) {
-        if (i == 0) {
-            e_1[i] = 1;
+// Def. of unitvector for as. a) (i)
+int[] normal_vector(int const dimension, int one_at_number) {
+   v[dimension];  // v could be zero-initialized, but that is indetermined
+   for(int i = 0; i <= dimension; ++i) {
+        if( i == one_at_number) {
+            v[i] = 1;
         }
         else {
-            e_1[i] = 0;
+            v[i] = 0;
         }
-        c_1[i] = 1;
-        v[i] = n - i;
-    }
+   }
+   return v
+}
 
-    for (int i = 0; i <= m - 1; ++i) {
-        for (int k = 0; k <= n - 1; ++k) {
-            if (i == k) {
-                I[k * m + i] = 1;
-                M[k * m + i] = 1;
+//Def. of one-vector for as. a) (ii)
+int[] one_vector(int const dimension) {
+    v[dimension];
+    for(int i = 0; i <= dimension; ++i) {
+        v[i] = 1;
+    }
+    return v;
+}
+
+// Def. of vector s.t. (a) (iii) is satisfied
+int[] vector_v(int const dimension) {
+    v[dimension];
+    for(int i = 0; i <= dimension; ++i) {
+        v[i] = dimension - i;
+    }
+    return v;
+}
+
+// Def. of 1-matrix for as. a) (iv)
+int[] one_matrix_coloum_major(int m, int n) {
+    I[m*n];
+    for(int i = 0; i <= m-1; ++i) {
+        for(int k = 0; k <= n-1; ++k) {
+            if(i == k) {
+                I[k*m+i] = 1;
             }
             else {
-                I[k * m + i] = 0;
-                M[k * m + i] = k+1 ;  
+                I[k*m+i] = 0;
             }
         }
     }
+    return I;
+}
 
-    for (int i = 0; i <= n; ++i) {
-        cout << e_1[i] << "\t" << c_1[i] << "\t" << v[i] << endl;
-    }
+int[] matrix_in_coloum_major(int m, int n) {
+    M[m*n];
+    for( int i = 0; i <= m-1; ++i) {
+        for( int k = 0; k <= n-1; ++k) {
+            M[k*m+i] =  
 
-    cout << "\n\n\n\n";
+int main() {
+    int const nv = 5; // Def. der Vektorengroesse nach Aufg.
+    int const n = 4; 
+    int const m = 4;
 
-    for (int i = 0; i <= m - 1; ++i) {
-        for (int k = 0; k <= n - 1; ++k) {
-            cout << I[k * m + i] << "\t";
-        }
-        cout << endl;
-    }
 
-    cout << "\n\n\n\n";
-
-    for (int i = 0; i <= m - 1; ++i) {
-        for (int k = 0; k <= n - 1; ++k) {
-            cout << M[k * m + i] << "\t";
-        }
-
-        cout << endl;
-    }
 }
