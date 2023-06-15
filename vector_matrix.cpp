f04a4fce28fb25d162aa759d702027c7f0811e77
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-06-15 22:20:14 +0200

    worked on implementation. First segmentation fault

--- vector_matrix.cpp
+++ vector_matrix.cpp
@@ -6,63 +6,91 @@ using namespace std;
 // Assignment 5.2
 
 // Def. of unitvector for as. a) (i)
-int[] normal_vector(int const dimension, int one_at_number) {
-   v[dimension];  // v could be zero-initialized, but that is indetermined
+int* normal_vector(int const dimension, int one_at_number) {
+   int* v[dimension];  // v could be zero-initialized, but that is indetermined
    for(int i = 0; i <= dimension; ++i) {
         if( i == one_at_number) {
-            v[i] = 1;
+            *v[i] = 1;
         }
         else {
-            v[i] = 0;
+            *v[i] = 0;
         }
    }
-   return v
+   return *v;
 }
 
 //Def. of one-vector for as. a) (ii)
-int[] one_vector(int const dimension) {
-    v[dimension];
+int* one_vector(int const dimension) {
+    int* v[dimension];
     for(int i = 0; i <= dimension; ++i) {
-        v[i] = 1;
+        *v[i] = 1;
     }
-    return v;
+    return *v;
 }
 
 // Def. of vector s.t. (a) (iii) is satisfied
-int[] vector_v(int const dimension) {
-    v[dimension];
+int* vector_v(int const dimension) {
+    int* v[dimension];
     for(int i = 0; i <= dimension; ++i) {
-        v[i] = dimension - i;
+        *v[i] = dimension - i;
     }
-    return v;
+    return *v;
 }
 
 // Def. of 1-matrix for as. a) (iv)
-int[] one_matrix_coloum_major(int m, int n) {
-    I[m*n];
+int* one_matrix_coloum_major(int const m, int const n) {
+    int* I[m*n];
     for(int i = 0; i <= m-1; ++i) {
         for(int k = 0; k <= n-1; ++k) {
             if(i == k) {
-                I[k*m+i] = 1;
+                *I[k*m+i] = 1;
             }
             else {
-                I[k*m+i] = 0;
+                *I[k*m+i] = 0;
             }
         }
     }
-    return I;
+    return *I;
 }
 
-int[] matrix_in_coloum_major(int m, int n) {
-    M[m*n];
+int* matrix_in_coloum_major(int const m, int const n) {
+    int* M[m*n];
     for( int i = 0; i <= m-1; ++i) {
         for( int k = 0; k <= n-1; ++k) {
-            M[k*m+i] =  
+            *M[k*m+i] = 1;
+        }
+    }
+    return *M;
+}
+
+void print_vector(int* v, int n) {
+   for(int i = 0; i <= n; ++i) {
+       cout << v[i] << "\t";
+   }
+   cout << endl;
+}
+
+void print_Matrix_coloum_major(int* M, int const m, int const n) {
+    for(int i = 0; i <= m-1; ++i) {
+        for(int k = 0; k <= n-1; ++k) {
+            cout << M[k*m+i] << "\t";
+        }
+        cout << "\n";
+    }
+    cout << endl;
+} 
 
 int main() {
     int const nv = 5; // Def. der Vektorengroesse nach Aufg.
     int const n = 4; 
     int const m = 4;
 
+    int* e = normal_vector(nv, 1);
+    int* I = one_matrix_coloum_major(m, n);
 
+    print_vector(e, nv);
+    
+    print_Matrix_coloum_major(I, m, n);
+
+    return 1;
 }
