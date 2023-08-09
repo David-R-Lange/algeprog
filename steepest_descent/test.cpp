c3d51c21526e2582fcbdc428cd8ba99c2e5c54a6
dave <renedavidbrueggemann@gmail.com>
2023-08-09 21:25:14 +0200

    expanded test

--- steepest_descent/test.cpp
+++ steepest_descent/test.cpp
@@ -3,18 +3,23 @@
 using namespace std;
 
 int main() {
-    double const x = -1;
+    double const x_test = -1;
 
     Function func;
     double f = 0;
     double df = 0;
 
 
-    func.eval(x, f, df);
+    func.eval(x_test, f, df);
 
     double x0 = 0.33;
 
     SteepestDescent sd(func);
     int i = sd.solve(x0);
-    cout << "x = " << sd.x() << "\n" << "i = " << i << endl;
+   
+    double const x = sd.x();
+
+    func.eval(x, f, df);
+    cout << "x = " << x << "\n" << "i = " << i << endl;
+    cout << "f(x) = " << f << "\tdf(x) = " << df << endl;
 }
