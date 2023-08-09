8948503deef668c484c6b0c4eeed95acb1c0c7d5
David-R-Lange <renedavidbrueggemann@gmail.com>
2023-08-09 17:34:29 +0200

    removed non-factorial calculation

--- binomial.cpp
+++ binomial.cpp
@@ -8,21 +8,6 @@ unsigned int bin_rec(unsigned int n, unsigned int k) {
     return bin_rec(n-1, k-1) + bin_rec(n-1, k);
 }
 
-int factorial(unsigned int n) {
-    return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-}
-
-unsigned int binomial_no_factorial(unsigned int n, unsigned int k) {
-    unsigned int binomial = 1;
-    for (unsigned int i = 0; i < k+1; ++i) {
-        binomial = binomial * (n-i);
-    }
-
-    binomial = binomial/factorial(k);
-
-    return binomial;
-}
-
 unsigned int binomial_productrule(unsigned int n, unsigned int k) {
     unsigned int prod = 1;
     for (unsigned int j = 1; j <= k; ++j) {
@@ -39,9 +24,7 @@ int main() {
 
     n = 49;
     k = 6;
-    
-    cout << "\nMit Fakultaet: \n" << n << " ueber " << k << " = " << bin_rec(n,k) << endl;
-    cout << "\nOhne Fakultaet: \n" << n << " ueber " << k << " = " << binomial_no_factorial(n,k) << endl;
-    cout << "\nMit dem Produkt: \n" << n << " ueber " << k << " = " << binomial_productrule(n,k) << endl;
 
+    cout << "\nMit Fakultaet: \n" << n << " ueber " << k << " = " << bin_rec(n,k) << endl;
+    cout << "\nMit dem Produkt: \n" << n << " ueber " << k << " = " << binomial_productrule(n,k) << endl;
 }
